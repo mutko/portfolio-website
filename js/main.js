18ad33b13c887bcb2a34be833a0a38283a05d1ef
Branko Bozovic <branko028@yahoo.com>
2019-07-09 14:53:37 +0200

    Work on about section

--- js/main.js
+++ js/main.js
@@ -16,33 +16,3 @@ overlay.addEventListener("click", function() {
   menu.classList.toggle("menu-open");
   hambBtn.classList.toggle("open");
 });
-
-// animatite skill meters
-let el = document.querySelector(".progress-outer");
-const isInViewport = elem => {
-  let bounding = elem.getBoundingClientRect();
-  return (
-    bounding.top >= 0 &&
-    bounding.left >= 0 &&
-    bounding.bottom <=
-      (window.innerHeight || document.documentElement.clientHeight) &&
-    bounding.right <=
-      (window.innerWidth || document.documentElement.clientWidth)
-  );
-};
-
-if (isInViewport(el)) {
-  console.log("Jeste");
-  document.querySelector(".progress-inner").classList.add("animation");
-}
-
-window.addEventListener(
-  "scroll",
-  function(e) {
-    if (isInViewport(el)) {
-      console.log("Jeste");
-      document.querySelector(".progress-inner").classList.add("animation");
-    }
-  },
-  false
-);
