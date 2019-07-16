c192c279568c19691232da778f0e87197fc8e854
Branko Bozovic <branko028@yahoo.com>
2019-07-16 13:15:22 +0200

    Changes on navigation

--- js/main.js
+++ js/main.js
@@ -2,11 +2,8 @@ const id = s => document.getElementById(s);
 const hambBtn = id("hamb");
 const overlay = id("overlay");
 const menu = id("links");
-const pageHeader = id("page-header");
-const navBtn1 = id("btn-about");
-
-let navBtns = document.querySelectorAll(".nav-list li");
-let links = Array.from(navBtns);
+const navBtns = document.querySelectorAll(".nav-list li");
+const links = Array.from(navBtns);
 
 // nav links actions
 for (let i = 0; i < links.length; i += 1) {
@@ -22,7 +19,6 @@ hambBtn.addEventListener("click", function() {
   overlay.classList.toggle("active");
   menu.classList.toggle("menu-open");
   hambBtn.classList.toggle("open");
-  // pageHeader.classList.toggle("slide-margin");
 });
 
 overlay.addEventListener("click", function() {
