370f83b376e12964a582ba5da167d4e67317bdeb
Branko Bozovic <branko028@yahoo.com>
2019-07-16 12:30:06 +0200

    New spa structure and menu

--- js/main.js
+++ js/main.js
@@ -3,12 +3,26 @@ const hambBtn = id("hamb");
 const overlay = id("overlay");
 const menu = id("links");
 const pageHeader = id("page-header");
+const navBtn1 = id("btn-about");
 
+let navBtns = document.querySelectorAll(".nav-list li");
+let links = Array.from(navBtns);
+
+// nav links actions
+for (let i = 0; i < links.length; i += 1) {
+  links[i].addEventListener("click", function() {
+    overlay.classList.toggle("active");
+    menu.classList.toggle("menu-open");
+    hambBtn.classList.toggle("open");
+  });
+}
+
+// hamburger and overlay actions
 hambBtn.addEventListener("click", function() {
   overlay.classList.toggle("active");
   menu.classList.toggle("menu-open");
   hambBtn.classList.toggle("open");
-  pageHeader.classList.toggle("slide-margin");
+  // pageHeader.classList.toggle("slide-margin");
 });
 
 overlay.addEventListener("click", function() {
@@ -17,4 +31,5 @@ overlay.addEventListener("click", function() {
   hambBtn.classList.toggle("open");
 });
 
+// init glide carousel
 new Glide(".glide").mount();
