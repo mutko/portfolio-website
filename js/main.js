29b507b4093132f636877be0ab9d39d617eb1e39
Branko Bozovic <branko028@yahoo.com>
2019-07-16 15:21:46 +0200

    Change document title if out focus

--- js/main.js
+++ js/main.js
@@ -30,3 +30,34 @@ overlay.addEventListener("click", function() {
 // init glide carousel
 // new Glide(".works-carousel").mount();
 new Glide(".clients-carousel").mount();
+
+// change title if tab out of focus
+let hidden, visibilityChange;
+
+if (typeof document.hidden !== "undefined") {
+  hidden = "hidden";
+  visibilityChange = "visibilitychange";
+} else if (typeof document.msHidden !== "undefined") {
+  hidden = "msHidden";
+  visibilityChange = "msvisibilitychange";
+} else if (typeof document.webkitHidden !== "undefined") {
+  hidden = "webkitHidden";
+  visibilityChange = "webkitvisibilitychange";
+}
+
+function handleVisibilityChange() {
+  if (document[hidden]) {
+    document.title = "Please check this before closing :)";
+  } else {
+    document.title = "Branko | Frontend developer";
+  }
+}
+
+if (typeof document.addEventListener === "undefined" || hidden === undefined) {
+  console.log(
+    "This demo requires a browser, such as Google Chrome or Firefox, that supports the Page Visibility API."
+  );
+} else {
+  document.addEventListener(visibilityChange, handleVisibilityChange, false);
+  handleVisibilityChange();
+}
